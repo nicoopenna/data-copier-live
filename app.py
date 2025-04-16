fb30458578de56c24ffbfcf9a4b091b6066b2f06
nicoopenna <nicolas.penna16@gmail.com>
2025-04-15 22:50:21 -0300

    Add logit to obtain tables to be loaded

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 import sys
 import os
 from config import DB_DETAILS
-
-
+from util import get_tables
 
 
 def main():
@@ -12,7 +11,9 @@ def main():
     """
     env = sys.argv[1]
     db_details = DB_DETAILS[env]
-    print(db_details)
+    tables = get_tables('table_list')
+    for table in tables['table_name']:
+        print(table)
 
 
 if __name__ == '__main__':
