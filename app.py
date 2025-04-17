2e06698e431ad6cbe9c121da89217255bf511ac5
nicoopenna <nicolas.penna16@gmail.com>
2025-04-16 21:35:36 -0300

    Mysql connector and test query

--- app.py
+++ app.py
@@ -2,18 +2,28 @@ import sys
 import os
 from config import DB_DETAILS
 from util import get_tables
-
+import mysql.connector as myc
+import pandas as pd
 
 def main():
     """
     Program takes at least one argument
-    Min 49
     """
     env = sys.argv[1]
     db_details = DB_DETAILS[env]
+
+    conn = myc.connect(user=db_details["SOURCE_DB"]["DB_USER"],
+                password=db_details["SOURCE_DB"]["DB_PASS"],
+                database=db_details["SOURCE_DB"]["DB_NAME"],
+                host=db_details["SOURCE_DB"]["DB_HOST"],
+                port=db_details["SOURCE_DB"]["DB_PORT"]
+                )
+    cursor = conn.cursor()
+    cursor.execute("select * from categories")
+    df = pd.DataFrame(data=cursor.fetchall(),columns=cursor.column_names)
+    print(df.head())
     tables = get_tables('table_list')
-    for table in tables['table_name']:
-        print(table)
+
 
 
 if __name__ == '__main__':
--- config.py
+++ config.py
@@ -3,14 +3,16 @@ DB_DETAILS = {
     'dev': {
         'SOURCE_DB': {
             'DB_TYPE': 'mysql',
-            'DB_HOST': '139.99.209.131',
+            'DB_HOST': '127.0.0.1',
+            "DB_PORT": "3307",
             'DB_NAME': 'retail_db',
             'DB_USER': os.environ.get('SOURCE_DB_USER'),
             'DB_PASS': os.environ.get('SOURCE_DB_PASS'),
         },
         'TARGET_DB': {
             'DB_TYPE': 'postgres',
-            'DB_HOST': '139.99.209.131',
+            'DB_HOST': '127.0.0.1',
+            "DB_PORT": "3307",
             'DB_NAME': 'retail_db',
             'DB_USER': os.environ.get('TARGET_DB_USER'),
             'DB_PASS': os.environ.get('TARGET_DB_PASS'),
