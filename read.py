6be74c70bab834710cc0ce217200249112f80d89
nicoopenna <nicolas.penna16@gmail.com>
2025-04-18 15:17:00 -0300

    Initial script for postgres

--- read.py
+++ read.py
@@ -2,14 +2,14 @@ from util import get_connection
 
 
 def read_table(db_details, table_name, limit=0):
-    SOURCE_DB = db_details['SOURCE_DB']
+    source_db = db_details['SOURCE_DB']
 
-    connection = get_connection(db_type=SOURCE_DB['DB_TYPE'],
-                                db_name=SOURCE_DB['DB_NAME'],
-                                db_host=SOURCE_DB['DB_HOST'],
-                                db_port=SOURCE_DB['DB_PORT'],
-                                db_user=SOURCE_DB['DB_USER'],
-                                db_pass=SOURCE_DB['DB_PASS'])
+    connection = get_connection(db_type=source_db['DB_TYPE'],
+                                db_name=source_db['DB_NAME'],
+                                db_host=source_db['DB_HOST'],
+                                db_port=source_db['DB_PORT'],
+                                db_user=source_db['DB_USER'],
+                                db_pass=source_db['DB_PASS'])
     cursor = connection.cursor()
     if limit == 0:
         query = f'SELECT * FROM {table_name}'
