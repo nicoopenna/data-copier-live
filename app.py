15e197adcd3ce97c06c831d2bb94a651cb0d0d82
nicoopenna <nicolas.penna16@gmail.com>
2025-04-18 15:08:38 -0300

    Connection functions and read for MySQL. Docker file with postgress

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 import sys
 import os
-from config import DB_DETAILS
-from util import get_tables
-import mysql.connector as myc
+from read import read_table
+from util import get_tables, load_db_details
 import pandas as pd
 
 def main():
@@ -10,19 +9,12 @@ def main():
     Program takes at least one argument
     """
     env = sys.argv[1]
-    db_details = DB_DETAILS[env]
-
-    conn = myc.connect(user=db_details["SOURCE_DB"]["DB_USER"],
-                password=db_details["SOURCE_DB"]["DB_PASS"],
-                database=db_details["SOURCE_DB"]["DB_NAME"],
-                host=db_details["SOURCE_DB"]["DB_HOST"],
-                port=db_details["SOURCE_DB"]["DB_PORT"]
-                )
-    cursor = conn.cursor()
-    cursor.execute("select * from categories")
-    df = pd.DataFrame(data=cursor.fetchall(),columns=cursor.column_names)
-    print(df.head())
+    db_details = load_db_details(env)
     tables = get_tables('table_list')
+    for table in tables:
+        data, column_names = read_table(db_details=db_details,
+                                        table_name=table)
+
 
 
 
--- util.py
+++ util.py
@@ -1,6 +1,60 @@
 import pandas as pd
+from mysql import connector as mc
+from mysql.connector import errorcode as ec
+from config import DB_DETAILS
+from psycopg2 import connect
 
 
 def get_tables(path):
     tables = pd.read_csv(path, sep=':')
     return tables.query('to_be_loaded == "yes"')
+
+def load_db_details(env):
+    return DB_DETAILS[env]
+
+def get_mysql_conn(db_host, db_port, db_name, db_user, db_pass):
+    try:
+        connection = mc.connect(user=db_user,
+                       password=db_pass,
+                       database=db_name,
+                       host=db_host,
+                       port=db_port
+                       )
+    except mc.Error as error:
+        if error.errno == ec.ER_ACCESS_DENIED_ERROR:
+            print("Invalid credentials")
+        else:
+            print(error)
+    return connection
+
+
+def get_postgres_conn(db_host, db_port, db_name, db_user, db_pass):
+    try:
+        connection = connect(user=db_user,
+                       password=db_pass,
+                       database=db_name,
+                       host=db_host,
+                       port=db_port
+                       )
+    except mc.Error as error:
+        if error.errno == ec.ER_ACCESS_DENIED_ERROR:
+            print("Invalid credentials")
+        else:
+            print(error)
+    return connection
+
+
+def get_connection(db_type, db_host, db_port, db_name, db_user, db_pass):
+    if db_type == 'mysql':
+        connection = get_mysql_conn(db_host = db_host,
+                                    db_port = db_port,
+                                    db_name = db_name,
+                                    db_user = db_user,
+                                    db_pass = db_pass)
+    if db_type == 'postgres':
+        connection = get_postgres_conn(db_host=db_host,
+                                    db_port=db_port,
+                                    db_name=db_name,
+                                    db_user=db_user,
+                                    db_pass=db_pass)
+    return connection
